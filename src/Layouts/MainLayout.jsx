5d90c96b1c19499612ae6909107e4343310b2724
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-10 23:35:53 +0600

    fix bg theme

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Header/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
 
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className="min-h-[calc(100vh-306px)]">
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
