357ae8ef2ca9afd943c439d8d0df2a582d3793ff
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-11 15:28:47 +0600

    updated swiper slide

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,28 +1,51 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
 
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+import bgImg1 from "../../assets/images/carousel1.jpg"
+import bgImg2 from "../../assets/images/carousel2.jpg"
+import bgImg3 from "../../assets/images/carousel3.jpg"
+import Slide from '../Slide/Slide'
 
 export default function Carousel() {
     return (
-        <>
+        <div className='container px-6 py-10 mx-auto'>
             <Swiper
-                cssMode={true}
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
                 navigation={true}
-                pagination={true}
-                mousewheel={true}
-                keyboard={true}
-                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                className="mySwiper"
+                modules={[Autoplay, Pagination, Navigation]}
+                className='mySwiper'
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>
+                    <Slide
+                        image={bgImg1}
+                        text='Get Your Web Development Projects Done in minutes'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slide
+                        image={bgImg2}
+                        text='Get Your Graphics Design Projects Done in minutes'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slide
+                        image={bgImg3}
+                        text='Start Your Digital Marketing Campaigns up n running'
+                    />
+                </SwiperSlide>
             </Swiper>
-        </>
-    );
+        </div>
+    )
 }
